4e3592c5e0535a304cd80d6b7a6a70490a391dce
supchyan <mikhailsupov@yandex.ru>
2025-10-11 14:43:42 +0300

    snake fixes

--- js/snake.js
+++ js/snake.js
@@ -27,12 +27,20 @@ const APPLE = {
 const STEP = PLAYER_OBJECT.offsetWidth;
 
 var isGameRunning = false;
+var isDead = false;
+
+var gameScore = 0;
 
 // tutorial message
-TUTORIAL_TXT.innerHTML =
-`USE <y>ARROWS</y> TO CONTROL THE SNAKE.
-        PRESS <y>X</y> TO EXIT.
+function getTutorialHintMessage() {
+return `USE <y>ARROWS</y> TO CONTROL THE SNAKE.
+        PRESS <y>X</y> TO EXIT.`;
+}
+function getTutorialDeadMessage() {
+return `GAME OVER. YOU RECEIVED <y>${gameScore}</y> POINTS.
+         PRESS <y>X</y> TO EXIT.
 `;
+}
 
 function startSnakeGame() {
     document.getElementById("terminal").style.visibility = "collapse";
@@ -53,6 +61,9 @@ function startSnakeGame() {
     respawnApple(5, 5);
 
     isGameRunning = true;
+    isDead = false;
+
+    gameScore = 0;
 }
 function stopSnakeGame() {
     document.getElementById("terminal").style.visibility = "visible";
@@ -63,68 +74,81 @@ function stopSnakeGame() {
 
 setInterval(() => {
     if (isGameRunning) {
-        // player ate an apple
-        if (PLAYER.POSITION.X == APPLE.POSITION.X && PLAYER.POSITION.Y == APPLE.POSITION.Y) {
-            respawnApple();
-            
-            // add a new player segment
-            const SEGMENT_OBJECT = document.createElement("div")
-            SEGMENT_OBJECT.classList.add("segment");
-            SEGMENTS_OBJ.appendChild(SEGMENT_OBJECT);
-        }
+        if (!isDead) {
+            TUTORIAL_TXT.innerHTML = getTutorialHintMessage();
 
-        if (PLAYER.DIRECTION == "RIGHT") {
-            PLAYER.POSITION.X += STEP;
-        }
-        if (PLAYER.DIRECTION == "LEFT") {
-            PLAYER.POSITION.X -= STEP;
-        }
-        if (PLAYER.DIRECTION == "UP") {
-            PLAYER.POSITION.Y -= STEP;
-        }
-        if (PLAYER.DIRECTION == "DOWN") {
-            PLAYER.POSITION.Y += STEP;
-        }
-
-        if (PLAYER.POSITION.X > window.innerWidth) {
-            PLAYER.POSITION.X = 0;
-        }
-        if (PLAYER.POSITION.X < 0) { 
-            // properly calculate back position 
-            // according to window size and step size
-            // like in `respawnApple()` method.
-            PLAYER.POSITION.X = Math.floor(window.innerWidth / STEP) * STEP;
-        }
-        if (PLAYER.POSITION.Y > window.innerHeight) {
-            PLAYER.POSITION.Y = 0;
-        }
-        if (PLAYER.POSITION.Y < 0) {
-            // properly calculate back position 
-            // according to window size and step size
-            // like in `respawnApple()` method.
-            PLAYER.POSITION.Y = Math.floor(window.innerHeight / STEP) * STEP;
-        }
-
-        // save a new player position to buffer
-        POSITION_BUFFER.push({X: PLAYER.POSITION.X, Y: PLAYER.POSITION.Y});
-
-        const SEGMENT_OBJECTS = document.getElementsByClassName("segment");
-        for (var i = 0; i < SEGMENT_OBJECTS.length; i++) {
-            var index = POSITION_BUFFER.length - 2 - i;
-            if (index >= 0) {
-                SEGMENT_OBJECTS[i].style.top = `${POSITION_BUFFER[index].Y}px`;
-                SEGMENT_OBJECTS[i].style.left = `${POSITION_BUFFER[index].X}px`;
+            // player ate an apple
+            if (PLAYER.POSITION.X == APPLE.POSITION.X && PLAYER.POSITION.Y == APPLE.POSITION.Y) {
+                respawnApple();
+                
+                // add a new player segment
+                const SEGMENT_OBJECT = document.createElement("div")
+                SEGMENT_OBJECT.classList.add("segment");
+                SEGMENTS_OBJ.appendChild(SEGMENT_OBJECT);
             }
-        }
 
-        // update in-game score
-        SCORE_OBJECT.innerHTML = `${SEGMENT_OBJECTS.length}`;
-        
-        PLAYER_OBJECT.style.top = `${PLAYER.POSITION.Y}px`;
-        PLAYER_OBJECT.style.left = `${PLAYER.POSITION.X}px`;
-        
-        APPLE_OBJECT.style.top = `${APPLE.POSITION.Y}px`;
-        APPLE_OBJECT.style.left = `${APPLE.POSITION.X}px`;
+            if (PLAYER.DIRECTION == "RIGHT") {
+                PLAYER.POSITION.X += STEP;
+            }
+            if (PLAYER.DIRECTION == "LEFT") {
+                PLAYER.POSITION.X -= STEP;
+            }
+            if (PLAYER.DIRECTION == "UP") {
+                PLAYER.POSITION.Y -= STEP;
+            }
+            if (PLAYER.DIRECTION == "DOWN") {
+                PLAYER.POSITION.Y += STEP;
+            }
+
+            if (PLAYER.POSITION.X > window.innerWidth) {
+                PLAYER.POSITION.X = 0;
+            }
+            if (PLAYER.POSITION.X < 0) { 
+                // properly calculate back position 
+                // according to window size and step size
+                // like in `respawnApple()` method.
+                PLAYER.POSITION.X = Math.floor(window.innerWidth / STEP) * STEP;
+            }
+            if (PLAYER.POSITION.Y > window.innerHeight) {
+                PLAYER.POSITION.Y = 0;
+            }
+            if (PLAYER.POSITION.Y < 0) {
+                // properly calculate back position 
+                // according to window size and step size
+                // like in `respawnApple()` method.
+                PLAYER.POSITION.Y = Math.floor(window.innerHeight / STEP) * STEP;
+            }
+
+            // save a new player position to buffer
+            POSITION_BUFFER.push({X: PLAYER.POSITION.X, Y: PLAYER.POSITION.Y});
+
+            const SEGMENT_OBJECTS = document.getElementsByClassName("segment");
+            for (var i = 0; i < SEGMENT_OBJECTS.length; i++) {
+                var index = POSITION_BUFFER.length - 2 - i;
+                if (index >= 0) {
+                    SEGMENT_OBJECTS[i].style.top = `${POSITION_BUFFER[index].Y}px`;
+                    SEGMENT_OBJECTS[i].style.left = `${POSITION_BUFFER[index].X}px`;
+
+                    if (POSITION_BUFFER[index].X == PLAYER.POSITION.X && POSITION_BUFFER[index].Y == PLAYER.POSITION.Y) {
+                        isDead = true;
+                    }
+                }
+            }
+
+            gameScore = SEGMENT_OBJECTS.length;
+
+            // update in-game score
+            SCORE_OBJECT.innerHTML = `${gameScore}`;
+            
+            PLAYER_OBJECT.style.top = `${PLAYER.POSITION.Y}px`;
+            PLAYER_OBJECT.style.left = `${PLAYER.POSITION.X}px`;
+            
+            APPLE_OBJECT.style.top = `${APPLE.POSITION.Y}px`;
+            APPLE_OBJECT.style.left = `${APPLE.POSITION.X}px`;
+        }
+        else { // player died
+            TUTORIAL_TXT.innerHTML = getTutorialDeadMessage();
+        }
     }
 }, 100);
 
